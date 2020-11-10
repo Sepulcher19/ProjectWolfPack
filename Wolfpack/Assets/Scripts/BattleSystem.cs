7b3736a6c5febede44c9f8c68c60d30a6e58f6f1
SR198889 <SR198889@falmouth.ac.uk>
2020-11-10 17:41:58 +0000

    Added the project files for the unity game. including multiple sounds and effects and place holder sprites. This also includes the base programming for turn based systems.

---
+++ Wolfpack/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattlePosition;
+    public Transform enemyBattlePosition;
+
+    public BattleState state;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public Text dialogueText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SetupBattle()
+    {
+        GameObject playerGO = Instantiate(playerPrefab, playerBattlePosition);
+        playerUnit = playerGO.GetComponent<Unit>();
+
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattlePosition);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.text = "A wild " + enemyUnit.Name + " approaches";
+    }
+}
---
+++ Wolfpack/Assets/Scripts/Unit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string Name;
+
+    public int level;
+    public int damage;
+    public int maxHP;
+    public int currentHP;
+
+}
