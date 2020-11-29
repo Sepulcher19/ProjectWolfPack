3daeec6f3f421db24412bc9364502079e921926a
J-Taylor <jmarcustaylor@gmail.com>
2020-11-29 18:35:43 +0000

    text changes

--- Wolfpack/Assets/Scripts/TextBox.cs
+++ Wolfpack/Assets/Scripts/TextBox.cs
@@ -10,11 +10,7 @@ public class TextBox : MonoBehaviour
 
     public Text T;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
 
     // Update is called once per frame
     void Update()
@@ -25,7 +21,7 @@ public class TextBox : MonoBehaviour
         }
         if (pageNumb == 1)
         {
-            T.text = "Aplha: Elder, the Winter has been long and dark. There is a new threat in our woods and we seek your advise...";
+            T.text = "Alpha: Elder, the Winter has been long and dark. There is a new threat in our woods and we seek your advice...";
         }
         if (pageNumb == 2)
         {
@@ -33,7 +29,7 @@ public class TextBox : MonoBehaviour
         }
         if (pageNumb == 3)
         {
-            T.text = "Aplha: The Betas of the pack bring... trubbling news.";
+            T.text = "Alpha: The Betas of the pack bring... troubling news.";
         }
         if (pageNumb == 4)
         {
@@ -41,7 +37,7 @@ public class TextBox : MonoBehaviour
         }
         if (pageNumb == 5)
         {
-            T.text = "Elder: So our old enemies show them selves...";
+            T.text = "Elder: So our old enemies show themselves...";
         }
         if (pageNumb == 6)
         {
@@ -49,15 +45,15 @@ public class TextBox : MonoBehaviour
         }
         if (pageNumb == 7)
         {
-            T.text = "Aplha: This is why i come to you Elder... you know this old enemie better than any of us!";
+            T.text = "Alpha: This is why I come to you Elder... you know this old enemy better than any of us!";
         }
         if (pageNumb == 8)
         {
-            T.text = "Elder: Carm yourself Aplha, this enemy only recognises one thing...";
+            T.text = "Elder: Calm yourself Alpha, this enemy only recognises one thing...";
         }
         if (pageNumb == 9)
         {
-            T.text = "Aplha: That being?...";
+            T.text = "Alpha: That being?...";
         }
         if (pageNumb == 10)
         {
@@ -65,19 +61,15 @@ public class TextBox : MonoBehaviour
         }
         if (pageNumb == 11)
         {
-            T.text = "Aplha: War it is then...";
+            T.text = "Alpha: War it is then...";
         }
         if (pageNumb == 12)
         {
-            T.text = "Aplha: Summon the Beta's, we must push them back before more damage is done.";
-        }
-        if (pageNumb == 13)
-        {
-            T.text = "Aplha: Summon the Beta's, we must push them back before more damage is done.";
+            T.text = "Alpha: Summon the Beta's, we must push them back before more damage is done.";
         }
         if (pageNumb == 14)
         {
-            SceneManager.LoadScene("SampleScene"); ;
+            SceneManager.LoadScene("SampleScene"); 
         }
     }
 }
